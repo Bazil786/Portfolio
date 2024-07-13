919d6656b9f246a1a0cf7d6ad44eb63b83645e52
Bazil786 <baziltauhid0786@gmail.com>
2024-07-13 21:30:01 +0530

    Third Commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -42,15 +42,15 @@ export const Skills = () => {
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter1} alt="Image" />
                                 <h5>Cyber Security</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter2} alt="Image" />
                                 <h5>Problem Solving</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter2} alt="Image" />
                                 <h5>Software Development</h5>
                             </div>
                         </Carousel>
